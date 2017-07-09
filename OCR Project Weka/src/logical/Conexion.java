6e7c3e3125e4c40bc68e77d61c4a750a249c6610
felixalguzman <felixlvl@gmail.com>
2017-07-09 13:06:26 -0400

    Agregando JAR

---
+++ OCR Project Weka/src/logical/Conexion.java
@@ -0,0 +1,36 @@
+package logical;
+
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class Conexion {
+
+	private Instances dataset ;
+	private DataSource source;
+
+	public Conexion(String absolutePath) throws Exception {
+		
+		source = new DataSource(absolutePath);
+		dataset = source.getDataSet();
+	}
+
+	public Instances getDataset() {
+		return dataset;
+	}
+
+	public void setDataset(Instances dataset) {
+		this.dataset = dataset;
+	}
+
+	public DataSource getSource() {
+		return source;
+	}
+
+	public void setSource(DataSource source) {
+		this.source = source;
+	}
+
+	public String Resumen(){
+		return dataset.toSummaryString();
+	}
+}
