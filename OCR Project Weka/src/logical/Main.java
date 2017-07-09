7a88c2e38d80bc73bb1eee7616fa61feac296afc
felixalguzman <felixlvl@gmail.com>
2017-07-09 15:13:41 -0400

    Creando Dataset

--- OCR Project Weka/src/logical/Main.java
+++ OCR Project Weka/src/logical/Main.java
@@ -1,5 +1,8 @@
 package logical;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import visual.MainWindow;
 
 public class Main {
@@ -7,7 +10,22 @@ public class Main {
 	public static void main(String[] args) {
 
 		MainWindow m = new MainWindow();
+		System.out.println("\n \n \n");
+		crearDataset();
 
+	}
+	
+	public static void crearDataset()
+	{
+		try{
+		    PrintWriter writer = new PrintWriter("Datasets/training.arff", "UTF-8");
+		    writer.println("%% Datos de entrenamiento\n");
+		    writer.println("@relation training \n \n");
+		    writer.println("@data");
+		    writer.close();
+		} catch (IOException e) {
+		   // do something
+		}
 	}
 
 }
