89438706f1afa3ee9213946a30d1b224811f095e
felixalguzman <felixlvl@gmail.com>
2017-07-09 21:10:55 -0400

    Subiendo

--- OCR Project Weka/src/logical/Main.java
+++ OCR Project Weka/src/logical/Main.java
@@ -11,21 +11,10 @@ public class Main {
 
 		MainWindow m = new MainWindow();
 		System.out.println("\n \n \n");
-		//crearDataset();
+		
 
 	}
 	
-	public static void crearDataset()
-	{
-		try{
-		    PrintWriter writer = new PrintWriter("Datasets/training.arff", "UTF-8");
-		    writer.println("%% Datos de entrenamiento\n");
-		    writer.println("@relation training \n \n");
-		    writer.println("@data");
-		    writer.close();
-		} catch (IOException e) {
-		   // do something
-		}
-	}
+	
 
 }
