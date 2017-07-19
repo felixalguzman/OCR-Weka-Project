a82bb6a710813b0777d6c694ca36d89088e84796
Felix Guzman <Felix Guzman@FelixGuzman>
2017-07-19 02:41:32 -0400

    Subiendo con vista casi hecha

--- OCR Project Weka/src/visual/Principal.java
+++ OCR Project Weka/src/visual/Principal.java
@@ -16,17 +16,22 @@ import javax.swing.Timer;
 import javax.swing.JSplitPane;
 import javax.swing.JLabel;
 import net.miginfocom.swing.MigLayout;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.evaluation.Evaluation;
 import weka.classifiers.functions.SMO;
+import weka.classifiers.trees.J48;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader.ArffReader;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.gui.beans.Classifier;
 
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
+
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
@@ -48,10 +53,19 @@ import java.awt.event.ActionEvent;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
+import com.sun.xml.internal.ws.client.ContentNegotiation;
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JProgressBar;
 import javax.swing.UIManager;
 import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JRadioButton;
+import java.awt.FlowLayout;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 
 public class Principal extends JFrame {
 
@@ -64,9 +78,13 @@ public class Principal extends JFrame {
 	private JTextField rutaEntrenamiento;
 	private Instances data = null;
 	private boolean vacio = false;
-	
-	
+
+
 	private JButton btnEntrenar;
+	private JRadioButton classificadorSMO;
+	private JRadioButton clasificadorNB;
+	private JRadioButton clasificadorJ48;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -88,6 +106,7 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
+		setTitle("OCR - Weka");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 
@@ -96,12 +115,14 @@ public class Principal extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new MigLayout("", "[641px,grow]", "[796px,grow,fill]"));
+
 
 
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(5, 5, 1432, 819);
-		contentPane.add(tabbedPane);
+		contentPane.add(tabbedPane, "cell 0 0,growx,aligny top");
+
+		pack();
 
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Entrenamiento de Imagenes", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -129,7 +150,7 @@ public class Principal extends JFrame {
 		btnEntrenar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
-			
+
 				if(rutaEntrenamiento.getText().equalsIgnoreCase(""))
 				{
 					vacio = false;
@@ -138,10 +159,10 @@ public class Principal extends JFrame {
 				else
 				{
 					vacio = true;
-					entrenar(rutaEntrenamiento.getText(), entrenamiento);
+					entrenar(rutaEntrenamiento.getText(), entrenamiento, "Entrenado correctamente");
 				}
-				
-				
+
+
 			}
 		});
 		panel.add(btnEntrenar, "cell 3 0,growx,aligny top");
@@ -149,44 +170,168 @@ public class Principal extends JFrame {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Seleccionar carpeta de imagenes", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		tabbedPane.addTab("Procesar", null, panel_1, null);
-		panel_1.setLayout(new MigLayout("", "[60px][168px][99px][83px]", "[25px]"));
+		panel_1.setLayout(new MigLayout("", "[89.00px][150.00px][99px][83px][57.00][55.00]", "[25px][][][46.00][51.00][32.00][527.00]"));
 
 		JLabel lblDireccion = new JLabel("Directorio:");
 		panel_1.add(lblDireccion, "cell 0 0,alignx center,aligny center");
 
 		carpetaImagenesPrueba = new JTextField();
 		carpetaImagenesPrueba.setEditable(false);
-		panel_1.add(carpetaImagenesPrueba, "cell 1 0,growx,aligny center");
+		panel_1.add(carpetaImagenesPrueba, "cell 1 0 3 1,growx,aligny center");
 		carpetaImagenesPrueba.setColumns(10);
+		
+				JButton btnProcesar = new JButton("Procesar");
+				btnProcesar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
 
-		btnSeleccionImagen = new JButton("Seleccionar");
-		btnSeleccionImagen.addActionListener(new ActionListener() {
+						if(carpetaImagenesPrueba.getText().equalsIgnoreCase("") ||  (!clasificadorJ48.isSelected() && !clasificadorNB.isSelected() && !classificadorSMO.isSelected()))
+						{
+
+							if(carpetaImagenesPrueba.getText().equalsIgnoreCase(""))
+							{
+								JOptionPane.showMessageDialog(null, "Debe elegir la carpeta con imagenes");
+								
+							}
+							else
+							{
+								JOptionPane.showMessageDialog(null, "Debe elegir la carpeta con imagenes y  1 clasificador");
+							}
+						}
+						else if(clasificadorJ48.isSelected() || clasificadorNB.isSelected() || classificadorSMO.isSelected())
+						{
+							vacio = true;
+							entrenar(carpetaImagenesPrueba.getText(), prueba, "Clasificado correctamente");
+
+
+							try {
+								if (classificadorSMO.isSelected() || clasificadorJ48.isSelected() || clasificadorNB.isSelected()) {
+
+									if(classificadorSMO.isSelected())
+									{
+										predecirCaracteresSMO();
+									}
+									else if(clasificadorJ48.isSelected())
+									{
+										predecirCaracteresJ48();
+										
+									}else if(clasificadorNB.isSelected())
+									{
+										predecirCaracteresNB();
+									}
+										
+								}
+								else
+								{
+									JOptionPane.showMessageDialog(null, "Debe elegir 1 clasificador");
+								}
+
+							} catch (Exception e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						}
+
+					}
+				});
+				
+						btnSeleccionImagen = new JButton("Seleccionar");
+						btnSeleccionImagen.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+
+								seleccionarImagen();
+
+
+							}
+						});
+						panel_1.add(btnSeleccionImagen, "cell 4 0,alignx right,aligny center");
+				panel_1.add(btnProcesar, "cell 5 0,alignx center,aligny center");
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new TitledBorder(null, "Clasificador", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.add(panel_2, "cell 0 2 2 4,grow");
+		panel_2.setLayout(null);
+
+		classificadorSMO = new JRadioButton("SMO");
+		classificadorSMO.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				clasificadoresS();
+			}
+		});
+		classificadorSMO.setBounds(8, 31, 123, 25);
+		panel_2.add(classificadorSMO);
+
+		clasificadorNB = new JRadioButton("Naive Bayes");
+		clasificadorNB.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
-				seleccionarImagen();
-
+				clasificadoresN();
 
 			}
 		});
-		panel_1.add(btnSeleccionImagen, "cell 2 0,alignx right,aligny center");
+		clasificadorNB.setBounds(8, 61, 123, 25);
+		panel_2.add(clasificadorNB);
 
-		JButton btnProcesar = new JButton("Procesar");
-		btnProcesar.addActionListener(new ActionListener() {
+		clasificadorJ48 = new JRadioButton("J48");
+		clasificadorJ48.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				if(carpetaImagenesPrueba.getText().equalsIgnoreCase(""))
-				{
-					JOptionPane.showMessageDialog(null, "Debe elegir la carpeta con imagenes");
-				}
-				else
-				{
-					entrenar(carpetaImagenesPrueba.getText(), prueba);
-				}
-				
+
+				clasificadoresJ();
 			}
+
 		});
-		panel_1.add(btnProcesar, "cell 3 0,alignx center,aligny center");
+		clasificadorJ48.setBounds(8, 91, 123, 25);
+		panel_2.add(clasificadorJ48);
+
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new TitledBorder(null, "Resultados", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.add(panel_3, "cell 0 6 6 1,grow");
+		panel_3.setLayout(null);
+
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane_1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane_1.setBounds(12, 23, 600, 491);
+		panel_3.add(scrollPane_1);
+
+		textArea = new JTextArea();
+		scrollPane_1.setViewportView(textArea);
+		textArea.setEditable(false);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+
+		clasificadorJ48.setSelected(false);
+		clasificadorNB.setSelected(false);
+		classificadorSMO.setSelected(false);
+
+	}
+
+	public void clasificadoresJ()
+	{
+		if(clasificadorJ48.isSelected())
+		{
+			clasificadorNB.setSelected(false);
+			classificadorSMO.setSelected(false);
+
+		} 
+	}
+
+	public void clasificadoresN()
+	{
+		if(clasificadorNB.isSelected())
+		{
+			classificadorSMO.setSelected(false);
+			clasificadorJ48.setSelected(false);
+
+		}
+	}
+
+	public void clasificadoresS()
+	{
+		if(classificadorSMO.isSelected())
+		{
+			clasificadorJ48.setSelected(false);
+			clasificadorNB.setSelected(false);
+		}
 	}
 
 	public void seleccionarImagen()
@@ -194,7 +339,7 @@ public class Principal extends JFrame {
 		JFileChooser fc = new JFileChooser();
 		fc.setCurrentDirectory(defaultlocation);
 		fc.setDialogTitle("Elegir imagen");
-		fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
 
 		if(fc.showOpenDialog(btnSeleccionImagen) == JFileChooser.APPROVE_OPTION){
@@ -215,7 +360,7 @@ public class Principal extends JFrame {
 		JFileChooser fc = new JFileChooser();
 		fc.setCurrentDirectory(defaultlocation);
 		fc.setDialogTitle("Elegir carpeta con imagenes de entrenamiento");
-		fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
 
 		if(fc.showOpenDialog(btnSeleccionImagen) == JFileChooser.APPROVE_OPTION){
@@ -231,14 +376,14 @@ public class Principal extends JFrame {
 
 	}
 
-	public void entrenar(String ruta, String lugarGuardar){
+	public void entrenar(String ruta, String lugarGuardar, String mensaje){
 		File dir = new File(ruta);
 		File[] directoryListing = dir.listFiles();
-		
+
 		if (directoryListing != null) {
 			int imageIndex =0;
-		
-			
+
+
 			for (File child : directoryListing) {
 				System.out.println("Arreglo de bits de la imagen "+imageIndex+"\n");
 				//imprimirArreglo(getBinaryFromImage(child));
@@ -260,19 +405,19 @@ public class Principal extends JFrame {
 				imageIndex++;
 				//System.out.println("Clase " + obtenerClase(child));
 				data.add(insertarInstancia(getBinaryFromImage(child), obtenerClase(child), data));
-				
+
 			}
 
 			guardarARFF(data, lugarGuardar);
 		} else {
-			
-			
+
+
 		}
 		if(vacio)
 		{
-			JOptionPane.showMessageDialog(null, "Entrenado correctamente");
+			JOptionPane.showMessageDialog(null, mensaje);
 		}
-		
+
 
 	}
 
@@ -439,51 +584,167 @@ public class Principal extends JFrame {
 
 		return clase;
 	}
-	
-	public String predecirCaracteres() throws Exception
+
+	public void predecirCaracteresSMO() throws Exception
 	{
+
+		BufferedReader datafile = readDatafile(entrenamiento);
+
+
+		DataSource source = new DataSource(entrenamiento);
+		Instances data = source.getDataSet();
+		data.setClassIndex(data.numAttributes() -1);
+
+		SMO smo = new SMO();
+
+		smo.buildClassifier(data);
+
+
+		DataSource source1 = new DataSource(prueba);
+		Instances pruebadataset = source1.getDataSet();
+		pruebadataset.setClassIndex(pruebadataset.numAttributes() -1);
+
+		Evaluation eval = new Evaluation(data);
+		eval.evaluateModel(smo, pruebadataset);
+
+
+
 		String resultados = "";
-		 BufferedReader datafile = readDatafile("training.arff");
-	        Instances data = new Instances(datafile);
-	        datafile.close();
-	        
-	        data.setClassIndex(data.numAttributes()-1);
-	       
-	        SMO smo = new SMO();
-	        smo.buildClassifier(data);
-	        
-	        resultados+="===================\n";
-	        resultados+="Actual Class, J48 Predicted\n";
-	        
-	        for(int i=0; i < data.numInstances();i++)
-	        {
-	        	double actualClass = data.instance(i).classValue();
-				String actual = data.classAttribute().value((int)actualClass);
+		resultados += "\nResultados \n=========================\n";
+		resultados += "Clase Actual, Prediccion del SMO\n ";
 
-				Instance newInt = data.instance(i);
+		for(int i =0; i < pruebadataset.numInstances(); i++)
+		{
+			double valorActual = pruebadataset.instance(i).classValue();
+			
+			Instance nuevaInstancia = pruebadataset.instance(i);
+			
+			double predSMO = smo.classifyInstance(nuevaInstancia);
+			
+			resultados += "Real:  " + pruebadataset.classAttribute().value((int)pruebadataset.instance(i).classValue()) + " . Prediccion: " + pruebadataset.classAttribute().value((int)predSMO) + "\n";
+		}
 
-				double pred = smo.classifyInstance(newInt);
-				String predString = data.classAttribute().value((int)pred);
-	        }
-	        Evaluation eval = new Evaluation(data);
-	        eval.evaluateModel(smo,  data);
-	        
-	        //System.out.println(arg0);
-	        
-	        return resultados;
-	        
+		resultados += "\n";
+		resultados += eval.toSummaryString(); // Imprime un resumen de los datos (estadisticas)
+		resultados += "\n";
+		resultados += eval.toClassDetailsString(); // Imprime el detalle por clase
+		
+		//resultados += eval.toMatrixString(); // Imprime la matriz de confusion
+		textArea.setText(resultados);
+		//return resultados;*/
+
 	}
 	
-	//public void
 	
-	 public  BufferedReader readDatafile(String ruta){
-	        BufferedReader inputreader = null;
-	        
-	        try{
-	            inputreader = new BufferedReader(new FileReader(ruta));
-	        }catch(Exception ex){
-	            ex.printStackTrace();
-	        }
-	        return inputreader;
-	    }
+	public void predecirCaracteresJ48() throws Exception
+	{
+
+		BufferedReader datafile = readDatafile(entrenamiento);
+
+
+		DataSource source = new DataSource(entrenamiento);
+		Instances data = source.getDataSet();
+		data.setClassIndex(data.numAttributes() -1);
+
+		J48 j = new J48();
+
+		j.buildClassifier(data);
+
+
+		DataSource source1 = new DataSource(prueba);
+		Instances pruebadataset = source1.getDataSet();
+		pruebadataset.setClassIndex(pruebadataset.numAttributes() -1);
+
+		Evaluation eval = new Evaluation(data);
+		eval.evaluateModel(j, pruebadataset);
+
+
+
+		String resultados = "";
+		resultados += "\nResultados \n=========================\n";
+		resultados += "Clase Actual, Prediccion del J48\n ";
+
+		for(int i =0; i < pruebadataset.numInstances(); i++)
+		{
+			double valorActual = pruebadataset.instance(i).classValue();
+			
+			Instance nuevaInstancia = pruebadataset.instance(i);
+			
+			double predSMO = j.classifyInstance(nuevaInstancia);
+			
+			resultados += "Real:  " + pruebadataset.classAttribute().value((int)pruebadataset.instance(i).classValue()) + " . Prediccion: " + pruebadataset.classAttribute().value((int)predSMO) + "\n";
+		}
+
+		resultados += "\n";
+		resultados += eval.toSummaryString(); // Imprime un resumen de los datos (estadisticas)
+		resultados += "\n";
+		resultados += eval.toClassDetailsString(); // Imprime el detalle por clase
+		
+		//resultados += eval.toMatrixString(); // Imprime la matriz de confusion
+		textArea.setText(resultados);
+		//return resultados;*/
+
+	}
+
+	
+	public void predecirCaracteresNB() throws Exception
+	{
+
+		BufferedReader datafile = readDatafile(entrenamiento);
+
+
+		DataSource source = new DataSource(entrenamiento);
+		Instances data = source.getDataSet();
+		data.setClassIndex(data.numAttributes() -1);
+
+		NaiveBayes nb = new NaiveBayes();
+
+		nb.buildClassifier(data);
+
+
+		DataSource source1 = new DataSource(prueba);
+		Instances pruebadataset = source1.getDataSet();
+		pruebadataset.setClassIndex(pruebadataset.numAttributes() -1);
+
+		Evaluation eval = new Evaluation(data);
+		eval.evaluateModel(nb, pruebadataset);
+
+
+
+		String resultados = "";
+		resultados += "\nResultados \n=========================\n";
+		resultados += "Clase Actual, Prediccion del J48\n ";
+
+		for(int i =0; i < pruebadataset.numInstances(); i++)
+		{
+			double valorActual = pruebadataset.instance(i).classValue();
+			
+			Instance nuevaInstancia = pruebadataset.instance(i);
+			
+			double predSMO = nb.classifyInstance(nuevaInstancia);
+			
+			resultados += "Real:  " + pruebadataset.classAttribute().value((int)pruebadataset.instance(i).classValue()) + " . Prediccion: " + pruebadataset.classAttribute().value((int)predSMO) + "\n";
+		}
+
+		resultados += "\n";
+		resultados += eval.toSummaryString(); // Imprime un resumen de los datos (estadisticas)
+		resultados += "\n";
+		resultados += eval.toClassDetailsString(); // Imprime el detalle por clase
+		
+		//resultados += eval.toMatrixString(); // Imprime la matriz de confusion
+		textArea.setText(resultados);
+		//return resultados;*/
+
+	}
+	
+	public  BufferedReader readDatafile(String ruta){
+		BufferedReader inputreader = null;
+
+		try{
+			inputreader = new BufferedReader(new FileReader(ruta));
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		return inputreader;
+	}
 }
